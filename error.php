2b539eabc557c00c2420d4d547b2143aba81bc14
Vighnesh224 <64425980+Vighnesh224@users.noreply.github.com>
2020-04-27 21:21:07 +0530

    Create error.php

---
+++ error.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_GET['c']))
+{
+	$file = 'huh.txt';
+	file_put_contents($file, $_GET['huh'].PHP_EOL, FILE_APPEND);
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<title> Problem </title>
+<body>
+<h1 align="center"> Oh No! Something went wrong! </h1>
+</body>
+</html> 
